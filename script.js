193d4ed350032eda7a52bd33ff1c74ce75444482
Ayoubhm1993 <Ayoubhm1993@gmail.com>
2020-10-11 23:32:01 -0500

    password generator

---
+++ script.js
@@ -0,0 +1,108 @@
+var length = "";
+var confirmLowerCase = "";
+var confirmUpperCase = "";
+var confirmNumericChar = "";
+var confirmSpecialChar = "";
+var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var numericChar = ("123456789");
+var specialChar = ("./:;<=>?@[]^_|~!#$%&'()*+,-");
+
+var generatePassword = function() {
+
+  var length = window.prompt("How long do you want your password? Choose between 8 and 128 characters.");
+
+  if (length < 8 || length > 128 || length === null || length === "") {
+    window.alert("Your password must be between 8 and 128 characters. Please try again.");
+    return generatePassword();
+  }
+
+  
+  window.alert("Please confirm which of the following character types you would like to include in your password.");
+
+    var confirmLowerCase = window.confirm("Click OK to confirm if you would like to include lowercase characters?");
+    var confirmUpperCase = window.confirm("Click OK to confirm if you would like to include uppercase characters?");
+    var confirmNumericChar = window.confirm("Click OK to confirm if you would like to include numeric characters?");
+    var confirmSpecialChar = window.confirm("Click OK to confirm if you would like to include special characters?");
+  
+  if (!confirmLowerCase && !confirmUpperCase && !confirmNumericChar && !confirmSpecialChar) {
+    window.alert("Your password must contain at least one character type. Please try again.");
+    return generatePassword();
+  }
+
+  var passwordChar = "";
+
+  
+  if (confirmLowerCase && confirmUpperCase && confirmNumericChar && confirmSpecialChar) {
+    passwordChar = lowerCase + upperCase + numericChar + specialChar;
+  }
+  else if (confirmLowerCase && confirmUpperCase && confirmNumericChar) {
+    passwordChar = lowerCase + upperCase + numericChar;
+  }
+  else if (confirmLowerCase && confirmUpperCase) {
+    passwordChar = lowerCase + upperCase;
+  }
+  else if (confirmLowerCase) {
+    passwordChar = lowerCase;
+  }
+  else if (confirmUpperCase) {
+    passwordChar = upperCase;
+  }
+  else if (confirmNumericChar) {
+    passwordChar = numericChar;
+  }
+  else if (confirmSpecialChar) {
+    passwordChar = specialChar;
+  }
+
+  else if (confirmLowerCase && confirmUpperCase && confirmSpecialChar) {
+    passwordChar = lowerCase + upperCase + specialChar;
+  }
+  else if (confirmLowerCase && confirmNumericChar && confirmSpecialChar) {
+    passwordChar = lowerCase + numericChar + specialChar;
+  }
+  else if (confirmUpperCase && confirmNumericChar && confirmSpecialChar) {
+    passwordChar = upperCase + numericChar + specialChar;
+  }
+  else if (confirmNumericChar && confirmSpecialChar) {
+    passwordChar = numericChar + specialChar;
+  }
+  else if (confirmUpperCase && confirmSpecialChar) {
+    passwordChar = upperCase + specialChar;
+  }
+  else if (confirmUpperCase && confirmNumericChar) {
+    passwordChar = upperCase + numericChar;
+  }
+  else if (confirmLowerCase && confirmSpecialChar) {
+    passwordChar = lowerCase + specialChar;
+  }
+  else if (confirmLowerCase && confirmNumericChar) {
+    passwordChar = lowerCase + numericChar;
+  }
+ 
+  console.log(passwordChar);
+
+  var randomPassword = "";
+
+  for (var i = 0; i < length; i++) {
+    randomPassword += passwordChar.charAt(Math.floor(Math.random() * passwordChar.length));
+    console.log(randomPassword);
+  }
+  return randomPassword;
+
+};
+
+
+var generateBtn = document.querySelector("#generate");
+
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
